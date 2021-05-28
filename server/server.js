b42b5570e8e8400ad4f9e565714b9b289849f153
aristotlema <aristotle.maule@hotmail.com>
2021-05-28 07:43:40 -0400

    server setup

---
+++ server/server.js
@@ -0,0 +1,19 @@
+const dotenv = require('dotenv');
+const express = require('express');
+const { sequelize } = require('./src/models');
+
+dotenv.config();
+const app = express();
+app.use(express.json());
+
+const port = process.env.SERVER_PORT || 4000;
+
+app.listen({ port: port }, async () => {
+	console.log('Runnin on port', port);
+	try {
+		await sequelize.authenticate();
+		console.log('Connected to Database successfully');
+	} catch (err) {
+		console.error('Connection to Database failed', err);
+	}
+});
