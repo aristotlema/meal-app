6c2c16000cac7152f9bc386499cd0532059d2f7c
aristotlema <aristotle.maule@hotmail.com>
2021-06-03 07:20:55 -0400

    shopping list model and migrations

---
+++ server/src/migrations/20210603110838-create-shopping-list.js
@@ -0,0 +1,36 @@
+'use strict';
+module.exports = {
+	up: async (queryInterface, DataTypes) => {
+		await queryInterface.createTable('shopping_list', {
+			id: {
+				allowNull: false,
+				autoIncrement: true,
+				primaryKey: true,
+				type: DataTypes.INTEGER,
+			},
+			title: {
+				type: DataTypes.STRING,
+				allowNull: false,
+			},
+			date_start: {
+				type: DataTypes.INTEGER,
+				allowNull: false,
+			},
+			date_end: {
+				type: DataTypes.INTEGER,
+				allowNull: false,
+			},
+			created_at: {
+				allowNull: false,
+				type: DataTypes.DATE,
+			},
+			updated_at: {
+				allowNull: false,
+				type: DataTypes.DATE,
+			},
+		});
+	},
+	down: async (queryInterface, Sequelize) => {
+		await queryInterface.dropTable('shopping_list');
+	},
+};
---
+++ server/src/models/shoppinglist.js
@@ -0,0 +1,50 @@
+'use strict';
+const { Model } = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+	class ShoppingList extends Model {
+		/**
+		 * Helper method for defining associations.
+		 * This method is not a part of Sequelize lifecycle.
+		 * The `models/index` file will call this method automatically.
+		 */
+		static associate(models) {
+			// define association here
+		}
+	}
+	ShoppingList.init(
+		{
+			id: {
+				allowNull: false,
+				autoIncrement: true,
+				primaryKey: true,
+				type: DataTypes.INTEGER,
+			},
+			title: {
+				type: DataTypes.STRING,
+				allowNull: false,
+			},
+			date_start: {
+				type: DataTypes.INTEGER,
+				allowNull: false,
+			},
+			date_end: {
+				type: DataTypes.INTEGER,
+				allowNull: false,
+			},
+			created_at: {
+				allowNull: false,
+				type: DataTypes.DATE,
+			},
+			updated_at: {
+				allowNull: false,
+				type: DataTypes.DATE,
+			},
+		},
+		{
+			sequelize,
+			modelName: 'ShoppingList',
+			tableName: 'shopping_list',
+		}
+	);
+	return ShoppingList;
+};
